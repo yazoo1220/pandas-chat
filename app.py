cbe830c3b33232a4e4f61c04df68c5d7bc500050
yazoo1220 <podman1220@hotmail.com>
2023-07-21 23:40:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -59,10 +59,12 @@ def load_data(uploaded_file):
         st.error(f"Unsupported file format: {ext}")
         return None
 
+
+st.cache_data(ttl="1h")
 def load_sample_data():
     # Define the path to the sample data file
     sample_data_path = "./sample_data.csv"
-    # Load the sample data using the appropriate file format function (assuming it's CSV)
+    # Load the sample data
     df = pd.read_csv(sample_data_path)
     return df
 
